cfaa53eebd5384f1eb5ad631cbe7d997d2575e6f
NieamulKabir <kabir000079@gmail.com>
2023-01-04 00:31:16 +0600

    token time chng

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' });
             res.send({ token });
         });
         //get service..
